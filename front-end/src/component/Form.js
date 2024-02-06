609bc1aaf979d83ad1531e0173355b69bb8f914b
Nikhil <nikhildgr788@gmail.com>
2024-02-06 13:23:47 +0530

    form validation achieved

--- front-end/src/component/Form.js
+++ front-end/src/component/Form.js
@@ -8,8 +8,15 @@ const Registration = () => {
     const [skills,setSkills] = useState("");
     const [workPlace,setworkPlace] = useState("");
     const [hobbies,setHobbies] = useState("");
+    const [error, setError] = useState(false);
 
     const collectData = async()=>{
+
+        if(!Firstname || !lastname || !education || !skills || !workPlace || !hobbies){
+            setError(true);
+            return false;
+        }
+
         console.log(Firstname,lastname,education,skills,workPlace,hobbies);
         let result = await fetch('http://localhost:5000/register', { 
             method: "post",
@@ -30,34 +37,43 @@ const Registration = () => {
                 <input type="text" id="firstname" value={Firstname} 
                 onChange={(e)=>setfirstName(e.target.value)} ></input>
                 <label for="firstname">First Name </label > 
-
+                
                 <input type="text" id="lastname" value={lastname}
                 onChange={(e)=>setlastName(e.target.value)} ></input>
                 <label className="lastname" for="lastname">Last Name : </label>
+                
+                <div className="invalidInput2">
+                {error && !Firstname && <span>Enter Valid Firstname</span> }
+                {error && !lastname && <span>Enter Valid Lastname</span> }
+                </div>
             </div>
 
             <div className="inputGroup">
                 <input type="text" id="education" value={education}
                 onChange={(e)=>setEducation(e.target.value)}></input>
                 <label for="education">Education </label>
+                {error && !education && <span className='invalidInput'>Enter Valid Education</span> }
             </div>
 
             <div className="inputGroup">
                 <input type="text" id="skills" value={skills}
                 onChange={(e)=>setSkills(e.target.value)}></input>
                 <label for="skills">skills </label>
+                {error && !skills && <span className='invalidInput'>Enter Valid Skills</span> }
             </div>
 
             <div className="inputGroup">
                 <input type="text" id="workPlace" value={workPlace}
                 onChange={(e)=>setworkPlace(e.target.value)}></input>
                 <label for="workPlace">Work Place </label>
+                {error && !workPlace && <span className='invalidInput'>Enter Valid Workplace</span> }
             </div>
 
             <div className="inputGroup">
                 <input type="text" id="hobbies" value={hobbies}
                 onChange={(e)=>setHobbies(e.target.value)}></input>
                 <label for="hobbies">Hobbies </label>
+                {error && !hobbies && <span className='invalidInput'>Enter Valid Hobbies</span> }
             </div>
 
             <button onClick={collectData} type="button">Click to proceed</button>
